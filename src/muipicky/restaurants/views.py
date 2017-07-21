f057bc505377f83a4fb829eeff78fb8b9a3d9260
Nikki Wong <NikkiWong@ecf.ie>
2017-07-21 14:39:51 +0100

    playing with 2 html blocks, displaying one or the other using conditional statements

--- src/muipicky/restaurants/views.py
+++ src/muipicky/restaurants/views.py
@@ -4,35 +4,36 @@ from django.shortcuts import render
 
 # Create your views here.
 # function based view
-
-# def home_old(request):
-#     # f strings are cool way to do substitutions! you need the f before the comment
-#     # and the {} where you want to sub
-#     html_var = 'f strings'
-#     html_ = f"""
-#     <!DOCTYPE html>
-#     <html lang=en>
-#     <head>
-#     </head>
-#     <body>
-#     <h1>Hello World!</h1>
-#     <p> This is {html_var} coming through </p>
-#     </body>
-#     </html>
-#     """
-       
-#     return HttpResponse(html_)
-#     #because it's a function based view it needs to return some sort of response
-    
+   
 def home(request):
-    num = random.randint(0, 10000000)
-    some_list = [num, random.randint(0, 10000000), random.randint(0, 10000000)]
+    num = 0
+    some_list = [
+        random.randint(0, 10000000),
+        random.randint(0, 10000000),
+        random.randint(0, 10000000)
+    ]
+    condition_bool_item = True
+    if condition_bool_item:
+        num = random.randint(0, 10000000)
     context = {
-        "bool_item": False,
         "num": num, 
         "some_list": some_list
     }
-    return render(request, "base.html", context) #response
+    return render(request, "home.html", context) #response
 #{} is a python dictionary.
 # Django uses logic that goes with the templates
 # it takes in request, some template, extra context and produces results.  
+
+  
+def home2(request):
+    context = {
+    }
+    return render(request, "home2.html", context) 
+
+
+
+def home3(request):
+    context = {
+
+    }
+    return render(request, "home3.html", context) 
