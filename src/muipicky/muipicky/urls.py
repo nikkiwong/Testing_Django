a8f11a3b014f9625246a5bae9dc295fd13677780
Nikki Wong <NikkiWong@ecf.ie>
2017-07-24 12:21:24 +0100

    using TemplateView in the urls.py to reduce the size of the views file

--- src/muipicky/muipicky/urls.py
+++ src/muipicky/muipicky/urls.py
@@ -15,15 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-from restaurants.views import home, about, contact, ContactView
+from restaurants.views import ContactView, HomeView, AboutView
 # function based views are lowercase and class based are uppercase
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home),
-    url(r'^about/$', about),
-    url(r'^contact/(?P<id>\d+)/$', ContactView.as_view()),
+    url(r'^$', HomeView.as_view()),
+    url(r'^about/$', TemplateView.as_view(template_name = 'about.html')),
+    url(r'^contact/(?P<id>\d+)/$', TemplateView.as_view(template_name = 'contact.html')),
 ]
 
 # because using class view need to create an instance of this class 
--- src/muipicky/restaurants/views.py
+++ src/muipicky/restaurants/views.py
@@ -3,44 +3,36 @@ import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
+# now its class based view!
 
-# Create your views here.
-# function based view
-   
-def home(request):
-    num = None
-    some_list = [
-        random.randint(0, 10000000),
-        random.randint(0, 10000000),
-        random.randint(0, 10000000)
-    ]
-    condition_bool_item = False
-    if condition_bool_item:
-        num = random.randint(0, 10000000)
-    context = {
-        "num": num, 
-        "some_list": some_list
-    }
-    return render(request, "home.html", context) #response
-#{} is a python dictionary.
-# Django uses logic that goes with the templates
-# it takes in request, some template, extra context and produces results.  
+class HomeView(TemplateView):
+    template_name = 'home.html'
+    # need to add overriding context method in TemplateView (a predefined method that comes with this class)
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        # using super to call the default context method on HomeView
+        # pass in args and kwargs as a fail safe
+        num = None
+        some_list = [
+            random.randint(0, 10000000),
+            random.randint(0, 10000000),
+            random.randint(0, 10000000)
+        ]
+        condition_bool_item = False
+        if condition_bool_item:
+            num = random.randint(0, 10000000)
+        context = {
+            "num": num, 
+            "some_list": some_list
+        }
+        return context
 
-  
-def about(request):
-    context = {
-    }
-    return render(request, "about.html", context) 
+class AboutView(TemplateView):
+    template_name = 'about.html'
 
-def contact(request):
-    context = {
-    }
-    return render(request, "contact.html", context) 
+class ContactView(TemplateView):
+    template_name = 'contact.html'
 
-class ContactView(View):
-    # need to define what kind of method this view is allows for (eg 'GET'/'POST')
-    def get(self, request, *args, **kwargs):
-        # because its a method inside of a class you need to have 'self' as a parameter
-        # get method is a type of request (getting info from the server)
-        context = {}
-        return render(request, "contact.html", context) 
+    # because these are just plain template views with not context then we can remove these in the view and 
+    # add them to the urls directly!
