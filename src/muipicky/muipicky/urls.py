5a3d59e817a135bb4b56796bee7577514a5133d4
Nikki Wong <NikkiWong@ecf.ie>
2017-07-25 11:02:07 +0100

    added objects using python shell, writing class based view (previous was function based view

--- src/muipicky/muipicky/urls.py
+++ src/muipicky/muipicky/urls.py
@@ -17,12 +17,19 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from restaurants.views import restaurant_listview
+from restaurants.views import (
+    restaurant_listview,
+    RestaurantListView,
+    MexicanRestaurantListView,
+    AsianFusionRestaurantListView,
+)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name = 'home.html')),
-    url(r'^restaurants/$', restaurant_listview),
+    url(r'^restaurants/$', RestaurantListView.as_view()),
+    url(r'^restaurants/mexican/$', MexicanRestaurantListView.as_view()),
+    url(r'^restaurants/asian/$', AsianFusionRestaurantListView.as_view()),
     url(r'^about/$', TemplateView.as_view(template_name = 'about.html')),
     url(r'^contact/(?P<id>\d+)/$', TemplateView.as_view(template_name = 'contact.html')),
 ]
--- src/muipicky/restaurants/views.py
+++ src/muipicky/restaurants/views.py
@@ -3,11 +3,11 @@ import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from .models import RestaurantLocation
 
-
+# functional based view
 def restaurant_listview(request):
     template_name = 'restaurants/restaurants_lists.html'
     queryset = RestaurantLocation.objects.all()
@@ -15,3 +15,16 @@ def restaurant_listview(request):
         "object_list": queryset
     } 
     return render(request, template_name, context)
+
+# class based view
+class RestaurantListView(ListView):
+    queryset = RestaurantLocation.objects.all()
+    template_name = 'restaurants/restaurants_lists.html'
+
+class MexicanRestaurantListView(ListView):
+    queryset = RestaurantLocation.objects.filter(category__iexact='mexican')
+    template_name = 'restaurants/restaurants_lists.html'
+
+class AsianFusionRestaurantListView(ListView):
+    queryset = RestaurantLocation.objects.filter(category__iexact='asian fusion')
+    template_name = 'restaurants/restaurants_lists.html'
