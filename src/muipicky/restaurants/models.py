c7854d6bf7ff0b874413561f842031cc4332bd1e
Nikki Wong <NikkiWong@ecf.ie>
2017-07-26 15:36:30 +0100

    using reverse shortcuts? basically using url names instead of the path for linking incase your path is very complicated as hardcoding them all the time would be hard to manage, also easy to change if its just using reference name

--- src/muipicky/restaurants/models.py
+++ src/muipicky/restaurants/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 
 from django.db import models
 from django.db.models.signals import pre_save, post_save
+from django.core.urlresolvers import reverse
 
 from .utils import unique_slug_generator
 from .validators import validate_category 
@@ -17,10 +18,14 @@ class RestaurantLocation(models.Model):
     updated         = models.DateTimeField(auto_now=True)
     # my_date_field   = models.DateField(auto_now=False, auto_now_add=False) 
     slug            = models.SlugField(null=True, blank=True)
-    
+
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        # return f"/restaurants/{self.slug}"
+        return reverse('restaurant-detail', kwargs={'slug': self.slug})
+        # kwargs = key word arguments. slug = key and self.slug = argument
     @property
     def title(self):
         return self.name # this allows us to write obj.title (as we have defined it as name ) 
