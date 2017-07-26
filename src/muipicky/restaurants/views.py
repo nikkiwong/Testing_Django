23548f1beea1b39d3e40c9d57d3e14ab3b78fb95
Nikki Wong <NikkiWong@ecf.ie>
2017-07-26 12:50:54 +0100

    using decorator (for function base view) and mixin (for class base view) to create a view that can only be accessible if the user is logged in, otherwise it will redirect user to a login page

--- src/muipicky/restaurants/views.py
+++ src/muipicky/restaurants/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
-import random
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
@@ -8,7 +9,6 @@ from django.views.generic import TemplateView, ListView, DetailView, CreateView
 from .forms import RestaurantCreateForm, RestaurantLocationCreateForm
 from .models import RestaurantLocation
 
-# functional based view
 def restaurant_listview(request):
     template_name = 'restaurants/restaurants_lists.html'
     queryset = RestaurantLocation.objects.all()
@@ -17,6 +17,7 @@ def restaurant_listview(request):
     } 
     return render(request, template_name, context)
 
+@login_required(login_url='/login/')
 def restaurant_createview(request):
     form = RestaurantLocationCreateForm(request.POST or None)
     errors = None
@@ -58,19 +59,22 @@ class RestaurantListView(ListView):
 class RestaurantDetailView(DetailView):
     queryset = RestaurantLocation.objects.all() 
 
-class RestaurantCreateView(CreateView):
+class RestaurantCreateView(LoginRequiredMixin, CreateView):
     form_class = RestaurantLocationCreateForm
+    login_url = '/login/'
     template_name = 'restaurants/form.html'
     success_url = "/restaurants/"
 
     def form_valid(self, form):
         instance = form.save(commit=False)
-        # can customize here
-        # pre_save
+        # making instance from form itself because its a createview so it HAS to be a model form, 
+        # not just a regular form. 
         instance.owner = self.request.user
         # the request does come through on every single class based view, 
         # but instead of being passed into individual functions we need to 
         # do self.request!
 
-        # instance.save()
+        # instance.save() <-- don't need this because by default CreateView runs a 
+        # form valid method, and at the end of that method, it does that save (in the line below 
+        # where you are returning the super). 
         return super(RestaurantCreateView, self).form_valid(form)
