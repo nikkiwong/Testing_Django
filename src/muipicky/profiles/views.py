072ee3acf9115b47cacfb3acb8d2bfd99b66c721
Nikki Wong <NikkiWong@ecf.ie>
2017-07-31 16:51:48 +0100

    creating a follow/unfollow button for user profiles

--- src/muipicky/profiles/views.py
+++ src/muipicky/profiles/views.py
@@ -1,12 +1,28 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import DetailView, View
 
 from menus.models import Item
 from restaurants.models import RestaurantLocation
 
+
+from .models import Profile
 User = get_user_model()
+
+class ProfileFollowToggle(LoginRequiredMixin, View): # endpoint
+    def post(self, request, *args, **kwargs):
+        # print(request.data)
+        # print(request.POST)
+        user_to_toggle = request.POST.get("username")
+        profile_ = Profile.objects.get(user__username__iexact=user_to_toggle)
+        user = request.user
+        if user in profile_.followers.all: 
+            profile_.followers.remove(user)
+        else:
+            profile_.followers.add(user)
+        return redirect("/u/nikki/")
 
 class ProfileDetailView(DetailView):
     # queryset = User.objects.filter(is_active=True)
@@ -22,22 +38,15 @@ class ProfileDetailView(DetailView):
         return get_object_or_404(User, username__iexact=username, is_active=True)
 
     def get_context_data(self, *args, **kwargs):
-        # updating the context data using super, and using args and kwargs as safe guards for context data
         context = super(ProfileDetailView, self).get_context_data(*args, **kwargs)
-        # print(context)
         user = context['user']
+        is_following = False
+        if user.profile in self.request.user.is_following.all():
+            is_following = True
+        context['is_following'] = is_following
         query = self.request.GET.get('q')
         item_exists = Item.objects.filter(user=user).exists()
-        # context location will be equal to the restaurants we already have
-        # get_object (method above) because its related to the actual user model itself.
-        qs = RestaurantLocation.objects.filter(owner=user).search(query) # with the .search(query) we dont need this if condition (below)
-        # if query:
-        #     qs = qs.search(query)  # created this .search(query) so we can have a more complex lookup. 
-            # this is its own new custom query set as far as the filter is concerned... but not good to have all of that logic there as 
-            # you might want to use it again somewhere else.. therefore can put it into models, inside a model manager.
-            # if you just use "qs = RestaurantLocation.objects.search(query)" then the user is no longer being passed at all, so it will give you
-            # the results of all users who chose that particular restaurant!
-
+        qs = RestaurantLocation.objects.filter(owner=user).search(query) 
         if item_exists and qs.exists(): 
             context['locations'] = qs
         return context
