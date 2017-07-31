98fa4121b98f89a3b6bfc069b051b6db9824c460
Nikki Wong <NikkiWong@ecf.ie>
2017-07-31 17:31:53 +0100

    creating homefeed page

--- src/muipicky/menus/views.py
+++ src/muipicky/menus/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import View, ListView, DetailView, CreateView, UpdateView
 
 from .forms import ItemForm
 from .models import Item
 
-class ItemListView(ListView):
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated():
+            return render(request, "home.html", {})
+        user = request.user
+        is_following_user_ids = [x.user.id for x in user.is_following.all()]
+        qs = Item.objects.filter(user__id__in=is_following_user_ids, public=True).order_by("-updated")[:3]  # getting all the items related to whether or not i'm following them
+        # all the user profiles we are following
+        # our users feed
+        return render(request, "menus/home-feed.html", {'object_list': qs})
+class ItemListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return Item.objects.filter(user=self.request.user)
 
-class ItemDetailView(DetailView):
+class ItemDetailView(LoginRequiredMixin, DetailView):
     def get_queryset(self):
         return Item.objects.filter(user=self.request.user)
 
