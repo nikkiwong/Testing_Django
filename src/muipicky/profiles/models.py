a788cc0a9973a63fadc89a576e0c0901ff37034c
Nikki Wong <NikkiWong@ecf.ie>
2017-08-01 16:02:24 +0100

    created activation link that users use to activate their profiles

--- src/muipicky/profiles/models.py
+++ src/muipicky/profiles/models.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.db import models
+from django.core.mail import send_mail
 from django.db.models.signals import post_save 
+from django.core.urlresolvers import reverse
+from .utils import code_generator
 
 User = settings.AUTH_USER_MODEL
 
@@ -20,6 +23,7 @@ class Profile(models.Model):
     user            = models.OneToOneField(User)  # the main user's (their profile), models.ForeignKey = default  
     followers       = models.ManyToManyField(User, related_name='is_following', blank=True)  # these followers and following will be unique for each user  
     # following       = models.ManyToManyField(User, related_name='following', blank=True)
+    activation_key  = models.CharField(max_length=120, blank=True, null=True)
     activated       = models.BooleanField(default=False) # default=false means that the profile is not activated by default
     timestamp       = models.DateTimeField(auto_now_add=True)
     updated         = models.DateTimeField(auto_now=True)
@@ -30,8 +34,26 @@ class Profile(models.Model):
         return self.user.username
 
     def send_activation_email(self):
-        print("activating")
-        pass
+        print("Activation")
+        if not self.activated:
+            self.activation_key = code_generator()  # generate key
+            self.save()
+            path_ = reverse('activate', kwargs={"code": self.activation_key})
+            subject = 'Activate Account'
+            from_email = settings.DEFAULT_FROM_EMAIL
+            message = f'Activate your account here: { path_ }'
+            recipient_list = [self.user.email]   # the email of the person we want to send to
+            html_message = f'<p>Activate your account here: { path_ }</p>'
+            print(html_message)
+            # sent_mail = send_mail(       # send some email here and return that object (sent_mail)
+            #                 subject, 
+            #                 message, 
+            #                 from_email, 
+            #                 recipient_list, 
+            #                 fail_silently=False, 
+            #                 html_message=html_message)      
+            sent_mail = False
+            return sent_mail
 
 def post_save_user_receiver(sender, instance, created, *args, **kwargs):
     # after user is saved or created, want to make sure the profile exists
